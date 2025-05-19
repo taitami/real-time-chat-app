665a1c8a7a38f45206a0deb3561b6ae68d4939a8
taitami <vladm2329@gmail.com>
2025-05-19 23:15:40 +0300

    integrate socket.io and configure CORS for local development

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { Server } from "socket.io";
 import {fileURLToPath} from "url";
 import path from "path";
 
@@ -12,3 +13,8 @@ const expressServer = app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
 });
 
+const io = new Server(expressServer, {
+    cors: {
+        origin: process.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
+});
