5e212c5b0062c1fcc01d938fb2a254c671b40226
taitami <vladm2329@gmail.com>
2025-05-20 16:38:56 +0300

    added message sending via WebSocket

--- server/public/app.js
+++ server/public/app.js
@@ -1,1 +1,14 @@
 const socket = io('ws://localhost:3500');
+
+function sendMessage(e) {
+    e.preventDefault()
+    const input = document.querySelector('input')
+    if (input.value) {
+        socket.emit('message', input.value)
+        input.value = ""
+    }
+    input.focus()
+};
+
+document.querySelector('form')
+    .addEventListener('submit', sendMessage);
