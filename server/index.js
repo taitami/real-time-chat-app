b523cb6e48c1e597acc71955f6ee1f0bb8fef65d
taitami <vladm2329@gmail.com>
2025-05-20 13:56:33 +0300

    added basic socket connection and message handling

--- server/index.js
+++ server/index.js
@@ -20,3 +20,11 @@ const io = new Server(expressServer, {
         origin: process.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
 });
+
+io.on('connection', socket => {
+    console.log(`user ${socket.id} connected`)
+
+    socket.on('message', data => {
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+})
