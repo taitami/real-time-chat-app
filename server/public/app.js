ce8a8d33cd13571eeb8940a9d27dfe55215f5fd9
taitami <vladm2329@gmail.com>
2025-05-20 18:23:02 +0300

    added real-time typing indicator

--- server/public/app.js
+++ server/public/app.js
@@ -1,21 +1,31 @@
 const socket = io('ws://localhost:3500');
 
+const activity = document.querySelector(".activity")
+const msgInput = document.querySelector('input')
+
 function sendMessage(e) {
     e.preventDefault()
-    const input = document.querySelector('input')
-    if (input.value) {
-        socket.emit('message', input.value)
-        input.value = ""
+    if (msgInput.value) {
+        socket.emit('message', msgInput.value)
+        msgInput.value = ""
     }
-    input.focus()
+    msgInput.focus()
 };
 
 document.querySelector('form')
     .addEventListener('submit', sendMessage);
 
 socket.on("message", (data) => {
+    activity.textContent = ""
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
 })    
 
+msgInput.addEventListener('keypress', () => {
+    socket.emit("activity", socket.id.substring(0, 5));
+});
+
+socket.on("activity", name => {
+    activity.textContent = `${name} is typing`
+})
