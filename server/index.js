2f713c1e3dbf7e1ee5693c48a1bbd5e935bcee07
taitami <vladm2329@gmail.com>
2025-05-20 12:22:48 +0300

    added middleware to serve static files from the public

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const expressServer = app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`)
 });
