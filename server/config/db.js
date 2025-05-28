807b5982693aa953b392ea6e6da95648fd73124a
taitami <vladm2329@gmail.com>
2025-05-28 17:36:23 +0300

    added db connection setup with mongoose and dotenv

---
+++ server/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error(err.message)
+        process.exit(1); 
+    }
+}
+
+export default connectDB;
