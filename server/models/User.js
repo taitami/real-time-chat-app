af2d7be9bafabaf0a43737a327f57b7a18d829bc
taitami <vladm2329@gmail.com>
2025-05-28 18:40:22 +0300

    implemented password encryption and verification methods

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,28 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     password: { type: String, required: true },
-    avatar: { type: String, default: 'default_avatar.png' },
+    avatar: { type: String, default: 'default_avatar.png' }, 
     onlineStatus: { type: Boolean, default: false },
     lastSeen: { type: Date },
     rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Room' }]
 }, { timestamps: true });
 
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 export default User;
