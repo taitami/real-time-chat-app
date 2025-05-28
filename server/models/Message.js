ed13f75bd13224249632d0e0d9fd617c39c5a903
taitami <vladm2329@gmail.com>
2025-05-28 19:18:50 +0300

    added message model

---
+++ server/models/Message.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
+    content: { type: String, required: true, trim: true },
+    type: { type: String, enum: ['text', 'image', 'file'], default: 'text' },
+}, { timestamps: true });
+
+const Message = mongoose.model('Message', messageSchema);
+export default Message;
