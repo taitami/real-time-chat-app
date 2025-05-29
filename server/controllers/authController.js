ed1976e27c49b1904583caf00fe7c72df637552f
taitami <vladm2329@gmail.com>
2025-05-29 16:04:34 +0300

    implemented user registration with JWT token generation

---
+++ server/controllers/authController.js
@@ -0,0 +1,42 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
+
+export const registerUser = async (req, res) => {
+    const {username, email, password} = req.body;
+    try {
+        const userExists = await User.findOne({ $or: [{email}, {username}] });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const user = await User.create({username, email, password})
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                token: generateToken(user._id),
+            });
+        } else {
+            res.status(400).json({ message: 'Invalid user data' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error during registration' });
+    }
+}
